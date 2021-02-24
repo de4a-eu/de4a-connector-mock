2f1fc908b21703dadd2bb9f612bc0f28fade0e8d
Andreas <andreas.j.berg@gmail.com>
2021-02-24 09:22:22 +0100

    Using DE4A-commons for marshalling and unmarshalling

--- src/main/java/eu/de4a/connector/mock/config/TestResponsesConfig.java
+++ src/main/java/eu/de4a/connector/mock/config/TestResponsesConfig.java
@@ -3,77 +3,69 @@ package eu.de4a.connector.mock.config;
 import eu.de4a.connector.mock.controller.MarshallException;
 import eu.de4a.edm.jaxb.de_usi.ResponseForwardEvidenceType;
 import eu.de4a.edm.jaxb.do_im.ResponseExtractEvidenceType;
-import eu.de4a.edm.jaxb.dr_idk.ResponseLookupEvidenceServiceDataType;
-import eu.de4a.edm.jaxb.dr_idk.ResponseLookupRoutingInformationType;
+import eu.de4a.edm.jaxb.idk.ResponseLookupEvidenceServiceDataType;
+import eu.de4a.edm.jaxb.idk.ResponseLookupRoutingInformationType;
 import eu.de4a.edm.jaxb.dr_usi.ResponseTransferEvidenceType;
+import eu.de4a.edm.xml.de4a.DE4AMarshaller;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 
-import static eu.de4a.edm.jaxb.do_im.ObjectFactory._ResponseExtractEvidence_QNAME;
 
 @Configuration
 @Slf4j
 public class TestResponsesConfig {
 
-    @Autowired
-    Jaxb2Marshaller marshaller;
-    @Autowired
-    JaxbConfig.Helper helper;
-
     @Bean
     public eu.de4a.edm.jaxb.dr_im.ResponseTransferEvidenceType getDR1IMresponse() throws IOException, MarshallException {
         Resource xml = new ClassPathResource("/xml-schemas/examples/DR1-IM-response.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.dr_im.ObjectFactory._ResponseTransferEvidence_QNAME);
+        return DE4AMarshaller.drImResponseMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public ResponseTransferEvidenceType DR1USIresponse() throws IOException, MarshallException {
         Resource xml = new ClassPathResource("/xml-schemas/examples/DR1-USI-response.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.dr_usi.ObjectFactory._ResponseTransferEvidence_QNAME);
+        return DE4AMarshaller.drUsiResponseMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public eu.de4a.edm.jaxb.do_usi.ResponseExtractEvidenceType DO1USIresponse() throws IOException, MarshallException {
         Resource xml = new ClassPathResource("/xml-schemas/examples/DO1-USI-response.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.do_usi.ObjectFactory._ResponseExtractEvidence_QNAME);
+        return DE4AMarshaller.doUsiResponseMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public ResponseForwardEvidenceType DE1USIresponse() throws IOException, MarshallException {
         Resource xml = new ClassPathResource("/xml-schemas/examples/DE1-USI-response.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.de_usi.ObjectFactory._ResponseForwardEvidence_QNAME);
+        return DE4AMarshaller.deUsiResponseMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public ResponseExtractEvidenceType DO1IMresponse() throws IOException, MarshallException {
         Resource xml = new ClassPathResource("/xml-schemas/examples/DO1-IM-response.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), _ResponseExtractEvidence_QNAME);
+        return DE4AMarshaller.doImResponseMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public eu.de4a.edm.jaxb.dt_usi.ResponseTransferEvidenceType DT1USIresponse() throws IOException, MarshallException {
         Resource xml = new ClassPathResource("/xml-schemas/examples/DT1-USI-response.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.dt_usi.ObjectFactory._ResponseTransferEvidence_QNAME);
+        return DE4AMarshaller.dtUsiResponseMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public ResponseLookupEvidenceServiceDataType DR1IDKresponseevidence() throws IOException, MarshallException {
-        Resource xml = new ClassPathResource("/xml-schemas/examples/DR1-IDK-response-evidence.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.dr_idk.ObjectFactory._ResponseLookupEvidenceServiceData_QNAME);
+        Resource xml = new ClassPathResource("/xml-schemas/examples/DR-DT1-IDK-response-evidence.xml");
+        return DE4AMarshaller.idkResponseLookupEvidenceServiceDataMarshaller().read(xml.getInputStream());
     }
 
     @Bean
     public ResponseLookupRoutingInformationType DR1IDKresponserouting() throws IOException, MarshallException {
-        Resource xml = new ClassPathResource("/xml-schemas/examples/DR1-IDK-response-routing.xml");
-        return helper.unmarshall(new StreamSource(xml.getInputStream()), eu.de4a.edm.jaxb.dr_idk.ObjectFactory._ResponseLookupRoutingInformation_QNAME);
+        Resource xml = new ClassPathResource("/xml-schemas/examples/DR-DT1-IDK-response-routing.xml");
+        return DE4AMarshaller.idkResponseLookupRoutingInformationMarshaller().read(xml.getInputStream());
     }
 
 }
--- src/main/java/eu/de4a/connector/mock/controller/DE4AController.java
+++ src/main/java/eu/de4a/connector/mock/controller/DE4AController.java
@@ -1,20 +1,19 @@
 package eu.de4a.connector.mock.controller;
 
-import eu.de4a.connector.mock.config.JaxbConfig;
 import eu.de4a.edm.jaxb.common.types.AckType;
 import eu.de4a.edm.jaxb.common.types.ErrorListType;
 import eu.de4a.edm.jaxb.common.types.ErrorType;
 import eu.de4a.edm.jaxb.de_usi.RequestForwardEvidenceType;
 import eu.de4a.edm.jaxb.de_usi.ResponseForwardEvidenceType;
-import eu.de4a.edm.jaxb.do_im.ObjectFactory;
 import eu.de4a.edm.jaxb.do_im.RequestExtractEvidenceType;
 import eu.de4a.edm.jaxb.do_im.ResponseExtractEvidenceType;
-import eu.de4a.edm.jaxb.dr_idk.RequestLookupEvidenceServiceDataType;
-import eu.de4a.edm.jaxb.dr_idk.RequestLookupRoutingInformationType;
-import eu.de4a.edm.jaxb.dr_idk.ResponseLookupEvidenceServiceDataType;
-import eu.de4a.edm.jaxb.dr_idk.ResponseLookupRoutingInformationType;
+import eu.de4a.edm.jaxb.idk.RequestLookupEvidenceServiceDataType;
+import eu.de4a.edm.jaxb.idk.RequestLookupRoutingInformationType;
+import eu.de4a.edm.jaxb.idk.ResponseLookupEvidenceServiceDataType;
+import eu.de4a.edm.jaxb.idk.ResponseLookupRoutingInformationType;
 import eu.de4a.edm.jaxb.dr_im.RequestTransferEvidenceType;
 import eu.de4a.edm.jaxb.dr_im.ResponseTransferEvidenceType;
+import eu.de4a.edm.xml.de4a.DE4AMarshaller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,7 +24,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.transform.stream.StreamSource;
 import java.io.InputStream;
 import java.util.List;
 
@@ -33,11 +31,6 @@ import java.util.List;
 @Slf4j
 @RequestMapping(consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
 public class DE4AController {
-
-    @Autowired
-    Jaxb2Marshaller marshaller;
-    @Autowired
-    JaxbConfig.Helper helper;
 
     @Autowired
     ResponseExtractEvidenceType do1imresp;
@@ -61,103 +54,94 @@ public class DE4AController {
 
     @PostMapping("/do1/im/extractevidence")
     public ResponseEntity<String> DO1ImRequestExtractEvidence(InputStream body) {
-        ObjectFactory of = new ObjectFactory();
-        try {
-            RequestExtractEvidenceType req = helper.unmarshall(new StreamSource(body), ObjectFactory._RequestExtractEvidence_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        var marshaller = DE4AMarshaller.doImRequestMarshaller();
+        RequestExtractEvidenceType req = marshaller.read(body);
+        if (req == null) {
+            //todo: get info from exception
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 ResponseExtractEvidenceType res = new ResponseExtractEvidenceType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setErrorList(errorListType);
-                return helper.marshall(of.createResponseExtractEvidence(res));
+                return DE4AMarshaller.doImResponseMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseExtractEvidence(do1imresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.doImResponseMarshaller().getAsString(do1imresp));
     }
 
     @PostMapping("/do1/usi/extractevidence")
     public ResponseEntity<String> DO1USIRequestExtractEvidence(InputStream body) {
-        eu.de4a.edm.jaxb.do_usi.ObjectFactory of = new eu.de4a.edm.jaxb.do_usi.ObjectFactory();
-        try {
-            eu.de4a.edm.jaxb.do_usi.RequestExtractEvidenceType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.do_usi.ObjectFactory._RequestExtractEvidence_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        eu.de4a.edm.jaxb.do_usi.RequestExtractEvidenceType req = DE4AMarshaller.doUsiRequestMarshaller().read(body);
+
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new eu.de4a.edm.jaxb.do_usi.ResponseExtractEvidenceType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setErrorList(errorListType);
                 res.setAck(AckType.KO);
-                return helper.marshall(of.createResponseExtractEvidence(res));
+                return DE4AMarshaller.doUsiResponseMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseExtractEvidence(do1usiresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.doUsiResponseMarshaller().getAsString(do1usiresp));
     }
 
     @PostMapping("/de1/usi/forwardevidence")
     public ResponseEntity<String> de1usiresp(InputStream body) {
-        eu.de4a.edm.jaxb.de_usi.ObjectFactory of = new eu.de4a.edm.jaxb.de_usi.ObjectFactory();
-        try {
-            RequestForwardEvidenceType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.de_usi.ObjectFactory._RequestForwardEvidence_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        RequestForwardEvidenceType req = DE4AMarshaller.deUsiRequestMarshaller().read(body);
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new ResponseForwardEvidenceType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setErrorList(errorListType);
                 res.setAck(AckType.KO);
-                return helper.marshall(of.createResponseForwardEvidence(res));
+                return DE4AMarshaller.deUsiResponseMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseForwardEvidence(de1usiresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.deUsiResponseMarshaller().getAsString(de1usiresp));
     }
 
     @PostMapping("/dr1/idk/lookupevidenceservicedata")
     public ResponseEntity<String> dr1idkevidenceresp(InputStream body) {
-        eu.de4a.edm.jaxb.dr_idk.ObjectFactory of = new eu.de4a.edm.jaxb.dr_idk.ObjectFactory();
-        try {
-            RequestLookupEvidenceServiceDataType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.dr_idk.ObjectFactory._RequestLookupEvidenceServiceData_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        RequestLookupEvidenceServiceDataType req = DE4AMarshaller.idkRequestLookupEvidenceServiceDataMarshaller().read(body);
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new ResponseLookupEvidenceServiceDataType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setErrorList(errorListType);
-                return helper.marshall(of.createResponseLookupEvidenceServiceData(res));
+                return DE4AMarshaller.idkResponseLookupEvidenceServiceDataMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseLookupEvidenceServiceData(dr1idkevidenceresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.idkResponseLookupEvidenceServiceDataMarshaller().getAsString(dr1idkevidenceresp));
     }
 
     @PostMapping("/dr1/idk/lookuproutinginformation")
     public ResponseEntity<String> dr1idkroutingresp(InputStream body) {
-        eu.de4a.edm.jaxb.dr_idk.ObjectFactory of = new eu.de4a.edm.jaxb.dr_idk.ObjectFactory();
-        try {
-            RequestLookupRoutingInformationType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.dr_idk.ObjectFactory._RequestLookupRoutingInformation_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        RequestLookupRoutingInformationType req = DE4AMarshaller.idkRequestLookupRoutingInformationMarshaller().read(body);
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new ResponseLookupRoutingInformationType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setError(error);
-                return helper.marshall(of.createResponseLookupRoutingInformation(res));
+                return DE4AMarshaller.idkResponseLookupRoutingInformationMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseLookupRoutingInformation(dr1idkroutingresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.idkResponseLookupRoutingInformationMarshaller().getAsString(dr1idkroutingresp));
     }
 
     @PostMapping("/dr1/im/transferevidence")
     public ResponseEntity<String> dr1imresp(InputStream body) {
-        eu.de4a.edm.jaxb.dr_im.ObjectFactory of = new eu.de4a.edm.jaxb.dr_im.ObjectFactory();
-        try {
-            RequestTransferEvidenceType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.dr_im.ObjectFactory._RequestTransferEvidence_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        RequestTransferEvidenceType req = DE4AMarshaller.drImRequestMarshaller().read(body);
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new ResponseTransferEvidenceType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
@@ -171,48 +155,44 @@ public class DE4AController {
                 res.setDataOwner(dr1imresp.getDataOwner());
                 res.setDataRequestSubject(dr1imresp.getDataRequestSubject());
                 res.setCanonicalEvidenceId(dr1imresp.getCanonicalEvidenceId());
-                return helper.marshall(of.createResponseTransferEvidence(res));
+                return DE4AMarshaller.drImResponseMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseTransferEvidence(dr1imresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.drImResponseMarshaller().getAsString(dr1imresp));
     }
 
     @PostMapping("/dr1/usi/transferevidence")
     public ResponseEntity<String> dr1usiresp(InputStream body) {
-        eu.de4a.edm.jaxb.dr_usi.ObjectFactory of = new eu.de4a.edm.jaxb.dr_usi.ObjectFactory();
-        try {
-            eu.de4a.edm.jaxb.dr_usi.RequestTransferEvidenceType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.dr_usi.ObjectFactory._RequestTransferEvidence_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        eu.de4a.edm.jaxb.dr_usi.RequestTransferEvidenceType req = DE4AMarshaller.drUsiRequestMarshaller().read(body);
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new eu.de4a.edm.jaxb.dr_usi.ResponseTransferEvidenceType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setErrorList(errorListType);
                 res.setAck(AckType.KO);
-                return helper.marshall(of.createResponseTransferEvidence(res));
+                return DE4AMarshaller.drUsiResponseMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseTransferEvidence(dr1usiresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.drUsiResponseMarshaller().getAsString(dr1usiresp));
     }
 
     @PostMapping("/dt1/usi/transferevidence")
     public ResponseEntity<String> dt1usiresp(InputStream body) {
-        eu.de4a.edm.jaxb.dt_usi.ObjectFactory of = new eu.de4a.edm.jaxb.dt_usi.ObjectFactory();
-        try {
-            eu.de4a.edm.jaxb.dt_usi.RequestTransferEvidenceType req = helper.unmarshall(new StreamSource(body), eu.de4a.edm.jaxb.dt_usi.ObjectFactory._RequestTransferEvidence_QNAME);
-        } catch (MarshallException ex) {
-            throw new EndpointException("could not unmarshall input", ex, (error) -> {
+        eu.de4a.edm.jaxb.dt_usi.RequestTransferEvidenceType req = DE4AMarshaller.dtUsiRequestMarshaller().read(body);
+        if (req == null) {
+            throw new EndpointException("could not unmarshall input", (error) -> {
                 var res = new eu.de4a.edm.jaxb.dt_usi.ResponseTransferEvidenceType();
                 ErrorListType errorListType = new ErrorListType();
                 List<ErrorType> errorList = errorListType.getError();
                 errorList.add(error);
                 res.setErrorList(errorListType);
                 res.setAck(AckType.KO);
-                return helper.marshall(of.createResponseTransferEvidence(res));
+                return DE4AMarshaller.dtUsiResponseMarshaller().getAsString(res);
             });
         }
-        return ResponseEntity.status(HttpStatus.OK).body(helper.marshall(of.createResponseTransferEvidence(dt1usiresp)));
+        return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.dtUsiResponseMarshaller().getAsString(dt1usiresp));
     }
 
 
