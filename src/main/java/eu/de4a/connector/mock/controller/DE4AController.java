21c2890c4e3c397d6445e4b353b9e328b39bd5ca
Philip Helger <philip@helger.com>
2021-03-02 16:01:30 +0100

    Changed namespace to ".iem"

--- src/main/java/eu/de4a/connector/mock/controller/DE4AController.java
+++ src/main/java/eu/de4a/connector/mock/controller/DE4AController.java
@@ -1,11 +1,11 @@
 package eu.de4a.connector.mock.controller;
 
-import eu.de4a.edm.jaxb.common.types.*;
-import eu.de4a.edm.jaxb.t42.LegalEntityType;
-import eu.de4a.edm.xml.de4a.DE4AMarshaller;
-import eu.de4a.edm.xml.de4a.DE4AResponseDocumentHelper;
-import eu.de4a.edm.xml.de4a.EDE4ACanonicalEvidenceType;
-import eu.de4a.edm.xml.de4a.t42.DE4AT42Marshaller;
+import eu.de4a.iem.jaxb.common.types.*;
+import eu.de4a.iem.jaxb.t42.LegalEntityType;
+import eu.de4a.iem.xml.de4a.DE4AMarshaller;
+import eu.de4a.iem.xml.de4a.DE4AResponseDocumentHelper;
+import eu.de4a.iem.xml.de4a.EDE4ACanonicalEvidenceType;
+import eu.de4a.iem.xml.de4a.t42.DE4AT42Marshaller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
