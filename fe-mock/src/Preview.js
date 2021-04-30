a189230a49b7b07ffdff42b4d963a738e21a8c9a
Andreas Berg <andreas.j.berg@gmail.com>
2021-04-30 14:24:14 +0200

    slight changes

--- fe-mock/src/Preview.js
+++ fe-mock/src/Preview.js
@@ -1,3 +1,4 @@
+import Markdown from 'markdown-to-jsx'
 import React from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import EvidenceTable from './EvidenceTable'
@@ -9,18 +10,18 @@ const Preview = ({ evidence, evidenceRoot, evidenceIgnore, acceptEvidence, rejec
             <h1>{translate('previewTitle')}</h1>
         </Col></Row>
         <Row><Col>
-            <p>{translate('explanation')}</p>
+            <Markdown>{translate('explanation')}</Markdown>
         </Col></Row>
         <Row><Col>
             <EvidenceTable evidence={evidence} evidenceRoot={evidenceRoot} evidenceIgnore={evidenceIgnore} translate={translate}/>
-        </Col></Row>
-        <Row><Col>
-            <p> {translate('legalText')} </p>
         </Col></Row>
         <Row>
             <Col><Button variant='secondary' onClick={rejectEvidence}>{translate('rejectButton')}</Button></Col>
             <Col><Button variant='primary' onClick={acceptEvidence}>{translate('acceptButton')}</Button></Col>
         </Row>
+        <Row><Col>
+            <Markdown>{translate('legalText')}</Markdown> 
+        </Col></Row>
         
     </Container>
 }
--- fe-mock/src/translate/en.js
+++ fe-mock/src/translate/en.js
@@ -1,6 +1,6 @@
 const trans_en = {
     "previewTitle": "Preview",
-    "explanation": "some text",
+    "explanation": "The following information has been retrieved from the business register. Please review the provided information. \n\n By selecting [Register] you allow this information to be used for the company registration at the procedure. Selecting [Deny] will stop the procedure and the information will not be registered. \n\n  The information is formatted according to the European standards, therefore some information might look different from the country specific presentation. ",
     "canonicalEvidenceFields": {
         "LegalEntity": "Legal entity",
         "CompanyName": "Company name",
@@ -16,9 +16,9 @@ const trans_en = {
         "RegisteredAddress": "Registered address",
         "PostalAddress": "Postal address"
     },
-    "acceptButton": "Accept",
-    "rejectButton": "Reject",
-    "legalText": "Some legal text"
+    "acceptButton": "Register",
+    "rejectButton": "Cancel",
+    "legalText": "The DE4A project (https://www.de4a.eu/about-project) has been funded by the European Commission. This project pilots specific procedures of the Single Digital Gateway Regulation - Article 14 of the SDGR specifies the requirement for an explicit request."
 }
 
 export default trans_en
