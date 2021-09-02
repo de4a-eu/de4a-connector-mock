435e46bff0bdf7157c3c93b51facd184be8a5c7c
Andreas <andreas.j.berg@gmail.com>
2021-09-02 12:15:31 +0200

    Iteration 2 usi redirect changes

--- src/main/java/eu/de4a/connector/mock/controller/DTController.java
+++ src/main/java/eu/de4a/connector/mock/controller/DTController.java
@@ -3,18 +3,14 @@ package eu.de4a.connector.mock.controller;
 import com.helger.commons.error.level.EErrorLevel;
 import eu.de4a.connector.mock.Helper;
 import eu.de4a.connector.mock.exampledata.DataOwner;
+import eu.de4a.iem.jaxb.common.types.RedirectUserType;
 import eu.de4a.iem.jaxb.common.types.RequestForwardEvidenceType;
 import eu.de4a.iem.jaxb.common.types.RequestTransferEvidenceUSIDTType;
 import eu.de4a.iem.xml.de4a.DE4AMarshaller;
 import eu.de4a.iem.xml.de4a.DE4AResponseDocumentHelper;
-import eu.de4a.iem.xml.de4a.EDE4ACanonicalEvidenceType;
 import eu.de4a.iem.xml.de4a.IDE4ACanonicalEvidenceType;
 import eu.de4a.kafkaclient.DE4AKafkaClient;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
@@ -24,12 +20,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Consumer;
+
+import static eu.de4a.connector.mock.Helper.sendRequest;
 
 @RestController
 @Slf4j
@@ -41,10 +35,12 @@ public class DTController {
     private boolean forwardUSI;
     @Value("${mock.dt.forward.de.usi:set-some-url}")
     private String forwardUSIUrl;
+    @Value("${mock.dt.forward.de.redirectuser.usi:set-some-url}")
+    private String forwardUserRedirectUSIUrl;
 
     @PostMapping("${mock.dt.endpoint.usi}")
     public ResponseEntity<String> dt1usiresp(InputStream body) throws MarshallException {
-        var marshaller = DE4AMarshaller.dtUsiRequestMarshaller(null);
+        var marshaller = DE4AMarshaller.dtUsiRequestMarshaller(IDE4ACanonicalEvidenceType.NONE);
         UUID errorKey = UUID.randomUUID();
         marshaller.readExceptionCallbacks().set((ex) -> {
             MarshallErrorHandler.getInstance().postError(errorKey, ex);
@@ -62,34 +58,33 @@ public class DTController {
             if (dataOwner == null) {
                 log.error("no such data owner, how the hell did this happen?!");
             }
-            sendDERequest(forwardUSIUrl, deRequest, log::error);
+            sendRequest(forwardUSIUrl, DE4AMarshaller.deUsiRequestMarshaller(IDE4ACanonicalEvidenceType.NONE).getAsInputStream(deRequest), log::error);
         }
 
         var res = DE4AResponseDocumentHelper.createResponseError(true);
         return ResponseEntity.status(HttpStatus.OK).body(DE4AMarshaller.dtUsiResponseMarshaller().getAsString(res));
     }
 
-    public static CompletableFuture<Boolean> sendDERequest(String recipient, RequestForwardEvidenceType request, Consumer<String> onFailure) {
-        HttpResponse deResp;
-        try {
-            deResp = Request.Post(recipient)
-                    .bodyStream(DE4AMarshaller.deUsiRequestMarshaller(null)
-                            .getAsInputStream(request), ContentType.APPLICATION_XML)
-                    .execute().returnResponse();
-        } catch (IOException ex) {
-            onFailure.accept(String.format("Failed to send request to de: %s", ex.getMessage()));
-            return CompletableFuture.completedFuture(false);
-        }
-        if (deResp.getStatusLine().getStatusCode() < 200 && deResp.getStatusLine().getStatusCode() >= 300) { // not a 2XX status code
-            onFailure.accept(String.format("Request sent to de got status code %s, request %s body: %s", deResp.getStatusLine().getStatusCode(),
-                    recipient,
-                    DE4AMarshaller
-                            .deUsiRequestMarshaller(null)
-                            .getAsString(request)));
-            return CompletableFuture.completedFuture(false);
-        }
-        log.debug("Successfully sent de request with id: {}", request.getRequestId());
+    @PostMapping("${mock.dt.endpoint.redirectuser.usi}")
+    public ResponseEntity<String> dt1UsiUserRedirect(InputStream body) throws MarshallException {
+        var marshaller = DE4AMarshaller.deUsiRedirectUserMarshaller();
 
-        return CompletableFuture.completedFuture(true);
+        UUID errorKey = UUID.randomUUID();
+        marshaller.readExceptionCallbacks().set((ex) -> {
+            MarshallErrorHandler.getInstance().postError(errorKey, ex);
+        });
+        RedirectUserType req = marshaller.read(body);
+        if (req == null) {
+            throw new MarshallException(errorKey);
+        }
+        DE4AKafkaClient.send(EErrorLevel.INFO,
+                String.format("Recieving USI RedirectUser, requestId: %s", req.getRequestId()));
+        log.debug("Recieved RedirectUser, requestId: {}, redirectUrl: {}", req.getRequestId(), req.getRedirectUrl());
+
+        if (forwardUSI) {
+            sendRequest(forwardUserRedirectUSIUrl, DE4AMarshaller.deUsiRedirectUserMarshaller().getAsInputStream(req), log::error);
+        }
+        var res = DE4AResponseDocumentHelper.createResponseError(true);
+        return ResponseEntity.ok().build();
     }
 }
